1ac79184faf8e5db8dcc2a0e634e4b27b1bd5fc7
sufanaadrian <115484136+sufanaadrian@users.noreply.github.com>
2022-12-09 10:46:38 +0200

    NavBar created
    -UI for mobile and desktop version
    -DarkMode/LightMode button

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,4 +1,12 @@
+import Navbar from "scenes/navbar";
+
+const { Box } = require("@mui/material");
+
 const HomePage = () => {
-    return (<div>homepage</div>)
-}
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
+};
 export default HomePage;
